7d0451fcdaea2bbd8b01807075eb3f70d30f753a
MG40 <35602017+MG40@users.noreply.github.com>
2023-03-16 14:15:18 +0800

    Delete git_repo.tf

--- git_repo.tf
+++
@@ -1,8 +0,0 @@
-# Refer Sagemaker code repository
-resource "aws_sagemaker_code_repository" "code_repository" {
-  code_repository_name = "nova-sagemaker-notebook"
-
-  git_config {
-    repository_url = "https://github.com/MG40/nova-sagemaker-notebook.git"
-  }
-}
