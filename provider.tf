c3a84deb392f0d704a4ab9ac3bb090df0ef39f12
MG40 <35602017+MG40@users.noreply.github.com>
2023-03-16 14:15:40 +0800

    Delete provider.tf

--- provider.tf
+++
@@ -1,8 +0,0 @@
-### This part is done only after the completion of the following:
-### - AWS Direct connect setup
-### - Storage setup
-
-# Region the resources will be created
-provider "aws" {
-  region = "ap-southeast-1"
-}
